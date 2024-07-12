91e9f48c40398404d63974f307fa11e953d2d73e
niten <niten@fudo.org>
2024-07-12 16:33:13 -0700

    Updated for 24.05

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nexus Crypto Library and Keygen";
 
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-23.11";
+    nixpkgs.url = "nixpkgs/nixos-24.05";
     utils.url = "github:numtide/flake-utils";
     helpers = {
       url = "github:fudoniten/fudo-nix-helpers";
